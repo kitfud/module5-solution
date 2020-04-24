1b9443f79269362f57d60a8dd5bddd7f4ca2b0e6
Kit Fuderich <Kit@chenruis-MacBook-Pro.local>
2020-04-23 23:43:07 -0700

    complted module 5 solution

--- js/script.js
+++ js/script.js
@@ -88,7 +88,7 @@ $ajaxUtils.sendGetRequest(
 
 function (responseText) {
     document.querySelector("#main-content")
-      .innerHTML = buildAndShowHomeHTML(responseText);
+      buildAndShowHomeHTML(responseText);
   }, // ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitly setting the flag to get JSON from server processed into an object literal
 });
@@ -107,9 +107,10 @@ function buildAndShowHomeHTML (categories) {
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
-      var random = chooseRandomCategory(categories);
-      var randomCategory = "" + random.short_name;
-    
+      var random = chooseRandomCategory(categories).short_name;
+     
+     
+     
      
      
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
@@ -124,7 +125,7 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
     
      
-      var homeHtmlToInsertIntoMainPage= insertProperty(homeHtml,"randomCategoryShortName",randomCategory);
+      var homeHtmlToInsertIntoMainPage= insertProperty(homeHtml,"randomCategoryShortName","'"+random+"'");
 
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
